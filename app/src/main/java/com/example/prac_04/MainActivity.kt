89ec2d4847a86260fc25c0b490aa9daaab408c81
jtility <leejewon97@gmail.com>
2023-03-29 11:42:03 +0900

    완성 실습1 : constraint -> linear

--- app/src/main/java/com/example/prac_04/MainActivity.kt
+++ app/src/main/java/com/example/prac_04/MainActivity.kt
@@ -2,16 +2,39 @@ package com.example.prac_04
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CheckBox
+import android.widget.ImageView
+import com.example.prac_04.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 //        인형 옷 입히기 (체크박스 체크 시, 해당하는 이미지 덧입히기)
 //        필요한 것 :  체크박스 각각의 이벤트리스너(?)
 //            어떤 객체가 이벤트를 발생시켰는지를 알아낸다.
 //            그 객체의 텍스트정보(이미지의 이름?)를 받아온다. : getIdentifier
 //            이미지 파일명과 체크박스 텍스트를 일치시켜 놓으면, 객체의 텍스트정보를 받아와서 옷을 입힐 수 있을 것
-
+    lateinit var binding: ActivityMainBinding
+    lateinit var checkBoxArray: ArrayList<CheckBox>
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        init()
+    }
+
+    fun init(){
+        binding.apply {
+            checkBoxArray = arrayListOf(check1, check2, check3, check4, check5, check6, check7, check8, check9, check10)
+            for (check in checkBoxArray){
+                check.setOnCheckedChangeListener { button, isChecked ->
+                    val imgID = resources.getIdentifier(button.text.toString(), "id", packageName)
+                    val imgView = findViewById<ImageView>(imgID)
+                    if (isChecked)
+                        imgView.visibility = View.VISIBLE
+                    else
+                        imgView.visibility = View.INVISIBLE
+                }
+            }
+        }
     }
 }
